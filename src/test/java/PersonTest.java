d0560e936180c3f46f090cb053f9e1e31f66d4b8
Miguel Cosano <miguecosano2001@uma.es>
2023-02-11 11:26:39 +0100

    Added basic content to PersonTest

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,5 +1,38 @@
 import org.junit.jupiter.api.Test;
-
+/**
+ * Class used to test the Person class.
+ *
+ * @author Miguel Angel Cosano Ramirez
+ */
 class PersonTest {
+    Person person;
 
+
+    @Test
+    void createPersonWithEmptyName() {
+    }
+
+    @Test
+    void createPersonWithNegativeAge() {
+    }
+
+    @Test
+    void createPersonWithAgeGreaterThan120() {
+    }
+
+    @Test
+    void createPersonWithIncorrectGender() {
+    }
+
+    @Test
+    void getEverageFromEmptyPersonList() {
+    }
+
+    @Test
+    void getEverageFromPersonListWithOnePerson() {
+    }
+
+    @Test
+    void getEverageFromPersonListWithTwoPerson() {
+    }
 }
