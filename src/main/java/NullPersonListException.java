6905d92800d0afa0a19d035365d9d44085606f7e
Miguel-Cosano <miguecosano2001@uma.es>
2023-02-13 18:55:03 +0100

    Some little modifications in NullPersonListException

--- src/main/java/NullPersonListException.java
+++ src/main/java/NullPersonListException.java
@@ -1,3 +1,8 @@
+/**
+ * This class is used to throw an exception when the person list is null.
+ *
+ * @author Miguel Angel Cosano Ramirez
+ */
 public class NullPersonListException extends RuntimeException{
     public NullPersonListException(String errorMessage){
         super(errorMessage);
